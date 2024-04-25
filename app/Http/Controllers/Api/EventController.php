2ada3535e96bf62309d34a59a5e5d13dc3111050
Gary11777 <iharushakou@gmail.com>
2024-04-25 21:52:22 +0300

    88 Throttling the API

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -18,6 +18,8 @@ class EventController extends Controller
     public function __construct()
     {
         $this->middleware('auth:sanctum')->except(['index', 'show']);
+        $this->middleware('throtlle:api')
+            ->only(['store', 'update', 'destroy']);
         $this->authorizeResource(Event::class, 'event');
     }
 
